43a58fed46345fe28af1774c3239eb70c93358a9
alaa872003 <alaa.moh.latif@gmail.com>
2026-03-31 15:52:31 +0200

    commit : change port

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -7,7 +7,7 @@ interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
 
 // 2. Create Base Instance
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001/api/v1",
   withCredentials: true, // 🔥 CRITICAL: Allows frontend to send/receive the refreshToken cookie
 });
 
