cfa6ef3a1a0b0f39e401e0f4a18ac6781c3f9b0c
ismail <ismailibrahim0074@gmail.com>
2026-03-31 11:40:57 +0200

    integrate : community integration

--- src/redux/features/communities/communitiesSlice.ts
+++ src/redux/features/communities/communitiesSlice.ts
@@ -1,20 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = {
-  items: [],
-  loading: false,
-  error: null,
-};
+import { fetchCommunities, toggleStatus, createCommunityAction } from "./communitiesThunk";
 
 const communitiesSlice = createSlice({
   name: "communities",
-  initialState,
-  reducers: {
-    setCommunities: (state, action) => {
-      state.items = action.payload;
-    },
+  initialState: { data: [] as any[], isLoading: false },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCommunities.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchCommunities.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(toggleStatus.fulfilled, (state, action) => {
+      const index = state.data.findIndex((item: any) => item._id === action.payload._id);
+      if (index !== -1) {
+        state.data[index] = action.payload;
+      }
+    });
+    builder.addCase(createCommunityAction.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data.unshift(action.payload);
+    });
   },
 });
 
-export const { setCommunities } = communitiesSlice.actions;
 export default communitiesSlice.reducer;
