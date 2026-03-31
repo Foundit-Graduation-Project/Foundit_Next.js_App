9b6fed5bdf8c3ff1d43b8848e3bc6c24fd0d72b1
ismail <ismailibrahim0074@gmail.com>
2026-03-31 11:40:27 +0200

    integate: transaction integration

--- src/redux/features/transactions/transactionsSelectors.ts
+++ src/redux/features/transactions/transactionsSelectors.ts
@@ -2,3 +2,7 @@ import { RootState } from "@/redux/store";
 
 export const selectTransactions = (state: RootState) => state.transactions.items;
 export const selectTransactionsLoading = (state: RootState) => state.transactions.loading;
+export const selectTransactionsStats = (state: RootState) => state.transactions.stats;
+export const selectTransactionsTotalCount = (state: RootState) => state.transactions.totalCount;
+export const selectTransactionsTotalPages = (state: RootState) => state.transactions.totalPages;
+export const selectTransactionsError = (state: RootState) => state.transactions.error;
--- src/redux/features/transactions/transactionsThunk.ts
+++ src/redux/features/transactions/transactionsThunk.ts
@@ -1,11 +1,25 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { transactionsApi } from "@/lib/api/transactions.api";
 
 export const fetchTransactions = createAsyncThunk(
   "transactions/fetchAll",
+  async ({ page, limit }: { page: number; limit: number }, { rejectWithValue }) => {
+    try {
+      const response = await transactionsApi.getTransactions(page, limit);
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const fetchTransactionStats = createAsyncThunk(
+  "transactions/fetchStats",
   async (_, { rejectWithValue }) => {
     try {
-      return [];
-    } catch (error) {
+      const response = await transactionsApi.getTransactionStats();
+      return response.data;
+    } catch (error: any) {
       return rejectWithValue(error);
     }
   }
