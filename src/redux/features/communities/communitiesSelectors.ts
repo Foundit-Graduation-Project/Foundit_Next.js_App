ad05770c581a05a47bfddeca41d22afcab8702d4
Ziad-Abdallah <ziad38107@gmail.com>
2026-04-05 01:26:39 +0200

    modified:   src/redux/features/communities/communitiesSelectors.ts

--- src/redux/features/communities/communitiesSelectors.ts
+++ src/redux/features/communities/communitiesSelectors.ts
@@ -1,4 +1,4 @@
 import { RootState } from "@/redux/store";
 
-export const selectCommunities = (state: RootState) => state.communities.items;
-export const selectCommunitiesLoading = (state: RootState) => state.communities.loading;
+export const selectCommunities = (state: RootState) => state.communities.data;
+export const selectCommunitiesLoading = (state: RootState) => state.communities.isLoading;
